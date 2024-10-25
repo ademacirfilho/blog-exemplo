2643114f35396accbd5e3d66d656e86422f4b6c0
Ademacir Filho <ademacirfilho@gmail.com>
2024-10-25 10:18:30 -0300

    atualizaçoes no blog

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('post/<int:post_id>', views.post, name='post'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,15 @@ def index(request):
         "posts": Post.objects.all(),
     }
     return render(request, "index.html", context)
+
+def post(request, post_id):
+    context = {
+        "post": Post.objects.get(pk=post_id),
+    }
+    return render(request, "post.html", context)
+
+def about(request):
+    return render(request, "about.html")
+
+def contact(request):
+    return render(request, "contact.html")
