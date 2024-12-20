761805b38b0670231a033db819d1b9005cc41b05
Ademacir Filho <ademacirfilho@gmail.com>
2024-12-20 10:22:04 -0300

    blog update

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.forms.models import model_to_dict
 from .models import Post, Blog, Mensagem
-from .forms import MensagemForm
+from .forms import MensagemForm, PostForm
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     context = {
@@ -22,6 +23,53 @@ def about(request):
         "blog": Blog.objects.first()
     }
     return render(request, "about.html", context)
+
+@login_required
+def new_post(request):
+    context = {
+        "blog": Blog.objects.first(),
+    }
+
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+        else:
+            context["form"] = form
+    else:
+        context["form"] = PostForm()
+
+    return render(request, "new_post.html", context)
+
+def editar_post(request, post_id):
+    poste = get_object_or_404(Post, pk=post_id)
+    context = {
+        "blog": Blog.objects.first(),
+        "form": PostForm(instance=poste)
+    }
+
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=poste)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+        else:
+            context["form"] = form
+
+    return render (request, "new_post.html", context)
+
+def deletar_post(request, post_id):
+    context = {
+        "blog": Blog.objects.first(),
+        "post" : get_object_or_404(Post, pk=post_id)
+    }
+
+    if request.method == 'POST':
+        context["mensagem"].delete()
+        return redirect('index')
+    else:
+        return render (request, "delete_post.html", context)
 
 def contact(request):
     context = {
@@ -51,15 +99,16 @@ def editar_mensagem(request, mensagem_id):
     mensagem = get_object_or_404(Mensagem, pk=mensagem_id)
     context = {
         "blog": Blog.objects.first(),
-        "form": MensagemForm(initial=model_to_dict(mensagem))
+        "form": MensagemForm(instance=mensagem)
     }
 
     if request.method == "POST":
-        form = MensagemForm(request.POST)
+        form = MensagemForm(request.POST, instance=mensagem)
         if form.is_valid():
             form.save()
-
-        return redirect('message')
+            return redirect('message')
+        else:
+            context["form"] = form
 
     return render (request, "contact.html", context)
 
